f6e6ee3bc349c68f61f6b6b00941964e43e73c6f
Great Hayat <greathayat@gmail.com>
2019-02-03 11:59:58 -0800

    Add GET routes

--- index.js
+++ index.js
@@ -1,8 +1,23 @@
 const express = require('express');
 const app = express();
 
+const courses = [
+    {id: 1, name: "Node.js"},
+    {id: 2, name: "Angular"},
+    {id: 3, name: "ReactJS"}
+]
 app.get("/", (req, res) =>{
-    res.send("Hello, World!")
-})
+    res.send("Hello, World!");
+});
 
-app.listen(3000, () => console.log(`Listing on port 3000...`))
+app.get("/api/courses", (req, res) =>{
+    res.send(courses);
+});
+
+app.get("/api/courses/:id", (req, res) =>{
+    const course = courses.find(c => c.id == req.params.id);
+    if(!course) res.status(404).send("The Course with given ID was not found!");
+    res.send(course);
+})
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Listing on port ${port}...`))
